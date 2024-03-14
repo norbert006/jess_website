ce26be28ace0d67152da439f362ea6884f9dd96e
Norbert Blazejewski (ZJAC006) <ZJAC006@live.rhul.ac.uk>
2024-03-14 20:27:10 +0000

    Changing a few more visual aspects.

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -17,26 +17,26 @@ const Gallery = () => {
     }
     const galleryImages = [Images.sample_image1, Images.sample_image2, Images.sample_image3]
 
-    const openInstagram = () => {
-        window.open("https://www.instagram.com/clayfulhands/", "_blank", "noreferrer");
+    const openWebsite = (url) => {
+        window.open(url, "_blank", "noreferrer");
       };
 
 
     return (
         <div className="app__gallery flex__center">
             <div className="app__gallery-content">
-                <SubHeading title="Instagram" />
-                <h1 className="headtext__cormorant">Photo Gallery</h1>
+                <SubHeading title="About Us" />
+                <h1 className="headtext__cormorant">Jess & Sydney</h1>
                 <p className="p__opersans" style={{ color: '#AAA', marginTop: '2rem' }}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-                <button type="button" className="custom__button">View More</button>
+                <button type="button" className="custom__button" onClick={() => openWebsite("https://www.facebook.com/clayfulhands")}>Find out more</button>
             </div>
 
             <div className="app__gallery-images">
                 <div className="app__gallery-images_container" ref={scrollRef}>
                     {galleryImages.map((image, index) => (
                         <div className="app__gallery-images_card flex__center" key={`gallery_image-${index + 1}`}>
-                            <img src={image} alt="gallery" onClick={() => openInstagram()}/>
-                            <BsInstagram className="gallery__image-icon" onClick={() => openInstagram()}/>
+                            <img src={image} alt="gallery" onClick={() => openWebsite("https://www.instagram.com/clayfulhands/")}/>
+                            <BsInstagram className="gallery__image-icon" onClick={() => openWebsite("https://www.instagram.com/clayfulhands/")}/>
                         </div>
                     ))}
                 </div>
