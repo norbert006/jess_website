3bfa98c28a51dc19b8c13e5cf1aecbc3df7188ea
Norbert Blazejewski <74923336+norbert006@users.noreply.github.com>
2024-07-10 22:06:19 +0100

    Attempting to add background for text in about us section

--- src/components/SubHeading/SubHeading.jsx
+++ src/components/SubHeading/SubHeading.jsx
@@ -1,12 +1,12 @@
-import React from 'react';
+// import React from 'react';
 
-import pottery_icon from '../../assets/pottery_icon.png'
+// // import pottery_icon from '../../assets/pottery_icon.png'
 
-const SubHeading = ({ title }) => (
-  <div style={{ marginBottom: '1rem' }}>
-    <p className="p__cormorant">{title}</p>
-    <img src={pottery_icon} alt="spoon_image" className="spoon__img" />
-  </div>
-);
+// const SubHeading = ({ title }) => (
+//   <div style={{ marginBottom: '1rem' }}>
+//     <p className="p__cormorant">{title}</p>
+//     <img src={pottery_icon} alt="spoon_image" className="spoon__img" />
+//   </div>
+// );
 
-export default SubHeading;
+// export default SubHeading;
