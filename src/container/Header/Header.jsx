03e4a959bb5425e42b4aeffaae36a92b4c0fe673
Norbert Blazejewski (ZJAC006) <ZJAC006@live.rhul.ac.uk>
2024-07-18 21:06:47 +0100

    Changed the header element so that it is now an automatic gallery when view on mobile

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,30 +1,102 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 import header_photo from '../../assets/header_photo.JPG'
 import sub_header_finger from '../../assets/sub_header_finger.JPG';
 import sub_header_hen from '../../assets/sub_header_hen.JPG';
 import sub_header_family from '../../assets/sub_header_family.JPG';
-import button1 from "../../assets/button1.svg"
-import button2 from "../../assets/button2.svg"
-import button3 from "../../assets/button3.svg"
+import button1 from "../../assets/button1.svg";
+import button2 from "../../assets/button2.svg";
+import button3 from "../../assets/button3.svg";
+import Images from "../../assets/Images";
+import { BsInstagram, BsArrowLeftShort, BsArrowRightShort } from 'react-icons/bs'
+
 import './Header.css'
 
-const Header = () => (
-    <header className='header'>
-        <div className='imageContainer'>
-            <img src={header_photo} alt="Header_image" className='mainImage' />
-        </div>
-        <div className='buttonsContainer'>
-            <img src={button1} alt="button" className='button'/>
-            <img src={button2} alt="button" className='button'/>
-            <img src={button3} alt="button" className='button'/>
-        </div>
-        <div className='thumbnailsContainer'>
-            <img src={sub_header_finger} alt="Subheader_image" className='thumbnail' />
-            <img src={sub_header_hen} alt="Subheader_image" className='thumbnail' />
-            <img src={sub_header_family} alt="Subheader_image" className='thumbnail' />
-        </div>
-    </header>
+const Header = () => {
+
+    const [width, setWidth] = useState(window.innerWidth);
+
+    function handleWindowSizeChange() {
+        setWidth(window.innerWidth);
+    }
+    useEffect(() => {
+        window.addEventListener('resize', handleWindowSizeChange);
+        return () => {
+            window.removeEventListener('resize', handleWindowSizeChange);
+        }
+    }, []);
+
+    const isMobile = width <= 768;
+
+    const scrollRef = React.useRef(null)
+    const scroll = (direction) => {
+        const { current } = scrollRef;
+
+        if (direction === 'left') {
+            current.scrollLeft -= width;
+        } else {
+            current.scrollLeft += width;
+
+            if (current.scrollLeft >= current.scrollWidth - current.clientWidth) {
+                current.scrollLeft = 0;
+            }
+        }
+
+       
+    }
+    const galleryImages = [Images.hen_image1, Images.eve_image1, Images.sub_header_finger]
+
+    const openWebsite = (url) => {
+        window.open(url, "_blank", "noreferrer");
+    };
+
+    // Auto scroll functionality
+    useEffect(() => {
+        const interval = setInterval(() => {
+            scroll('right');
+        }, 3000); // Adjust the interval time as needed (e.g., 3000ms for 3 seconds)
+
+        return () => clearInterval(interval);
+    }, []);
+
+
+    if (isMobile) {
+        return (
+            <div className='top_container'>
+                <div className="header_gallery_images_container" style={{ marginTop: '0rem' }}>
+                    <div className="header_gallery_images no_scroll" ref={scrollRef}>
+                        {galleryImages.map((image, index) => (
+                            <div className="header_images_card flex__center" key={`gallery_image-${index + 1}`}>
+                                <img src={image} alt="gallery" onClick={() => openWebsite("https://www.instagram.com/clayfulhands/")} />
+                                <BsInstagram className="gallery__image-icon" onClick={() => openWebsite("https://www.instagram.com/clayfulhands/")} />
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            </div>
+        )
+    }
+    else {
+        return (
+            <header className='header'>
+                <div className='imageContainer'>
+                    <img src={header_photo} alt="Header_image" className='mainImage' />
+                </div>
+                <div className='buttonsContainer'>
+                    <img src={button1} alt="button" className='button' />
+                    <img src={button2} alt="button" className='button' />
+                    <img src={button3} alt="button" className='button' />
+                </div>
+                <div className='thumbnailsContainer'>
+                    <img src={sub_header_finger} alt="Subheader_image" className='thumbnail' />
+                    <img src={sub_header_hen} alt="Subheader_image" className='thumbnail' />
+                    <img src={sub_header_family} alt="Subheader_image" className='thumbnail' />
+                </div>
+            </header>
+        )
+    }
+
+
 
 
 
@@ -44,6 +116,6 @@ const Header = () => (
     //     </div>
 
     // </div>
-)
+}
 
 export default Header;
