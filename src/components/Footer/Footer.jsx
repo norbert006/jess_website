6c325cc267da23523f0a7f0104e9589f4154d602
Norbert Blazejewski (ZJAC006) <ZJAC006@live.rhul.ac.uk>
2024-09-02 19:54:35 +0100

    Updated footer styling

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import clayfulhands_logo from '../../assets/clayfulhands_logo.png'
-import clayfulhands_logo_transparent from '../../assets/clayfulhands_logo_transparent.png'
+import clayfulhands_logo_white from '../../assets/ClayfulWhite.png'
 import { FiFacebook, FiInstagram } from 'react-icons/fi'
 import './Footer.css'
 
@@ -10,12 +10,12 @@ const Footer = () => {
         <div className="app__footer section__padding">
             <div className="app__footer-links">
                 <div className="app__footer-links_logo">
-                    <img src={clayfulhands_logo_transparent} alt="footer_logo" />
-                    <p className="p__opensans">"At Clayful Hands, we believe in the magic of creativity, the joy of 
-                        getting messy, and the power of clay to inspire confidence and connection. Whether young or old, 
+                    <img src={clayfulhands_logo_white} alt="footer_logo" />
+                    <p className="p__opensans" style={{ color: 'aliceblue' }}>"At Clayful Hands, we believe in the magic of creativity, the joy of
+                        getting messy, and the power of clay to inspire confidence and connection. Whether young or old,
                         everyone deserves the time and space to explore, create, and have fun!"</p>
-                    {/* <img src={line_breaker} alt="footer_line_breaker" classname="spoon_img" stlye={{marginTop: 15}} /> Find some sort of a linebreaker image */}
-                    <div className="app__footer-links_icons">
+                    <div className="app__footer-links_icons" style={{ color: 'aliceblue' }}>
+                    <br></br>
                         <FiFacebook />
                         <FiInstagram />
                     </div>
@@ -23,7 +23,7 @@ const Footer = () => {
                 </div>
             </div>
             <div className="footer__copyright">
-                <p className="p__opensans">2024 Clayful Hands</p>
+                <p className="p__opensans" style={{ color: 'aliceblue' }}>2024 Clayful Hands</p>
             </div>
         </div>
     )
