26ee84d8cd4dee7f8a158bf8cdc4ec55519b060d
Norbert Blazejewski <74923336+norbert006@users.noreply.github.com>
2024-09-07 23:56:52 +0100

    Updates to the about us look

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -3,22 +3,16 @@ import { BsInstagram, BsArrowLeftShort, BsArrowRightShort } from 'react-icons/bs
 import SubHeading from '../../components/SubHeading/SubHeading';
 import Images from "../../assets/Images"
 import './Gallery.css';
+import '../InfoBox/InfoBox.css'
 
 const Gallery = ({ aboutUsRef }) => {
-
-    // useEffect(() => {
-    //     if (aboutUsRef.current) {
-    //       console.log('Gallery rendered and ref assigned:', aboutUsRef.current);
-    //     } else {
-    //       console.error('Gallery ref is still null after rendering');
-    //     }
-    //   }, [aboutUsRef]);
+    console.log("width = ", window.innerWidth)
 
     return (
         <div className="about-us-container" ref={aboutUsRef}>
             <h2 className="about-us-title">ABOUT US</h2>
             <div className="about-us-content" >
-                <div className="about-us-text" >
+                <div className='about_para'>
                     <p> Founded in 2023 by Jess and Sydney, Clayful Hands is all about sharing our passion for clay with others. We're dedicated 
                         to creating a space where everyone can explore this versatile medium, unleash their creativity, and have fun in the process. 
                         Based in East Sussex, Clayful Hands offers a welcoming environment for all, whether you're new to pottery or a seasoned enthusiast. 
