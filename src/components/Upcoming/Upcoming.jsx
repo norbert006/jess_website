9fefb993c79d4668f1c12f52662d4656f7431f37
Norbert Blazejewski (ZJAC006) <ZJAC006@live.rhul.ac.uk>
2024-09-11 22:18:47 +0100

    Copied and adjust the default view for a mobile device in the Upcoming component.

--- src/components/Upcoming/Upcoming.jsx
+++ src/components/Upcoming/Upcoming.jsx
@@ -39,12 +39,49 @@ const Upcoming = ({ upcomingEventsRef }) => {
 
     if (isMobile) {
         return (
-            <h1>IS MOBILE</h1>
+            <div className='background_image_phone' ref={upcomingEventsRef}>
+                <div className='upcoming_bg upcoming__wrapper upcoming_padding'>
+
+                    <div className='upcoming_wrapper_info'>
+                        <img src={upcoming_header} alt="heading_text" className='upcoming_header' />
+
+                        <div className='upcoming_content'>
+                            <div className='upcoming_content_quote upcomingMobile'>
+                                <p className='infoBox_para_mobile'>Book a Private Event with Clayful Hands
+                                    Looking for a unique and creative way to celebrate? Whether it's a hen or stag do, 
+                                    team-building session, or Christmas party, Clayful Hands offers tailored pottery experiences 
+                                    just for you. Choose from hand-building, pottery painting, or even a combination of both, 
+                                    including the chance to try your hand at the potter's wheel.
+                                </p>
+                            </div>
+                            <p className='upcoming_para_mobile upcomingMobile'>For more information on pricing and availability, get in touch with us today and let's plan an unforgettable event!</p>
+                        </div>
+
+                        <div className='upcoming_chef_sign'>
+                            <div className="upcoming_gallery_images" style={{ marginTop: '0rem' }}>
+                                <div className="upcoming_gallery_images_container" ref={scrollRef}>
+                                    {galleryImages.map((image, index) => (
+                                        <div className="upcoming_gallery_images_card flex__center" key={`gallery_image-${index + 1}`}>
+                                            <img src={image} alt="gallery" onClick={() => openWebsite("https://www.instagram.com/clayfulhands/")} />
+                                            <BsInstagram className="gallery__image-icon" onClick={() => openWebsite("https://www.instagram.com/clayfulhands/")} />
+                                        </div>
+                                    ))}
+                                </div>
+                                <div className="app__gallery-images_arrows">
+                                    <BsArrowLeftShort transform="shrink-6" className="gallery__arrow-icon" onClick={() => scroll('left')} />
+                                    <BsArrowRightShort className="gallery__arrow-icon" onClick={() => scroll('right')} />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
         )
     } else {
         return (
             <div className='upcoming_bg upcoming__wrapper upcoming_padding' ref={upcomingEventsRef}>
-                <div className='upcoming__wrapper_info upcoming'>
+                <div className='upcoming_wrapper_info upcoming'>
                     <img src={upcoming_header} alt="heading_text" className='upcoming_header' />
                     <p className='upcoming_para'>Upcoming Pottery Events: Get Creative with Clayful Hands!
                     Discover our latest workshops and experiences—perfect for all skill levels & abilities!
