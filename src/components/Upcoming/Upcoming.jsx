53d7ac447f0c6509fcb331d058cb742eb1201b2f
Norbert Blazejewski <74923336+norbert006@users.noreply.github.com>
2024-10-14 21:04:16 +0100

    Adjustments to resizing everything

--- src/components/Upcoming/Upcoming.jsx
+++ src/components/Upcoming/Upcoming.jsx
@@ -83,7 +83,7 @@ const Upcoming = ({ upcomingEventsRef }) => {
 
                         <div className='upcoming_content'>
                             <div className='upcoming_content_quote upcomingMobile'>
-                                <p className='infoBox_para_mobile'>Book a Private Event with Clayful Hands
+                                <p className='upcoming_para_mobile'>Book a Private Event with Clayful Hands
                                     Looking for a unique and creative way to celebrate? Whether it's a hen or stag do,
                                     team-building session, or Christmas party, Clayful Hands offers tailored pottery experiences
                                     just for you. Choose from hand-building, pottery painting, or even a combination of both,
@@ -96,8 +96,8 @@ const Upcoming = ({ upcomingEventsRef }) => {
                         <div className="gallery-container">
                             {galleryImages.map((item, index) => (
                                 <div key={index} className="gallery-item flex__center">
-                                    <img src={item.image} alt={item.name} className="gallery-image" onClick={() => openWebsite(item.url)}/>
-                                    <BsInstagram className="gallery__image-icon" onClick={() => openWebsite(item.url)} style={{ color: "var(--color-jess-green)" }}/>
+                                    <img src={item.image} alt={item.name} className="gallery-image" onClick={() => openWebsite(item.url)} />
+                                    <BsInstagram className="gallery__image-icon" onClick={() => openWebsite(item.url)} style={{ color: "var(--color-jess-green)" }} />
                                     <div className="image-details">
                                         <h3 className="image-title">{item.title}</h3>
                                         <p className="image-name">{item.name}</p>
@@ -116,9 +116,15 @@ const Upcoming = ({ upcomingEventsRef }) => {
             <div className='upcoming_bg upcoming__wrapper upcoming_padding' ref={upcomingEventsRef}>
                 <div className='upcoming_wrapper_info upcoming'>
                     <img src={upcoming_header} alt="heading_text" className='upcoming_header' />
-                    <p className='upcoming_para'>Upcoming Pottery Events: Get Creative with Clayful Hands!
-                        Discover our latest workshops and experiences—perfect for all skill levels & abilities!
+
+                    <p className='upcoming_para'>Book a Private Event with Clayful Hands
+                        Looking for a unique and creative way to celebrate? Whether it's a hen or stag do,
+                        team-building session, or Christmas party, Clayful Hands offers tailored pottery experiences
+                        just for you. Choose from hand-building, pottery painting, or even a combination of both,
+                        including the chance to try your hand at the potter's wheel.
                     </p>
+                    <p className='upcoming_para upcoming'>For more information on pricing and availability, get in touch with us today and let's plan an unforgettable event!</p>
+
                 </div>
 
                 <div className="upcoming_gallery_images">
@@ -131,8 +137,8 @@ const Upcoming = ({ upcomingEventsRef }) => {
                         ))}
                     </div>
                     <div className="app__gallery-images_arrows">
-                        <BsArrowLeftShort transform="shrink-2" className="gallery__arrow-icon" onClick={() => scroll('left')} style={{ backgroundColor: "var(--color-jess-green)" }}/>
-                        <BsArrowRightShort className="gallery__arrow-icon" onClick={() => scroll('right')} style={{ backgroundColor: "var(--color-jess-green)" }}/>
+                        <BsArrowLeftShort transform="shrink-2" className="gallery__arrow-icon" onClick={() => scroll('left')} style={{ backgroundColor: "var(--color-jess-green)" }} />
+                        <BsArrowRightShort className="gallery__arrow-icon" onClick={() => scroll('right')} style={{ backgroundColor: "var(--color-jess-green)" }} />
                     </div>
                 </div>
             </div>
