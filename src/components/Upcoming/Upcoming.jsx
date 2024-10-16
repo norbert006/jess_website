d63f5756d3a2ca09548de46aa9e7e8c6a10dfe92
Norbert Blazejewski (ZJAC006) <ZJAC006@live.rhul.ac.uk>
2024-10-16 19:27:14 +0100

    Changed the image gallery in the Upcoming section to now appear as a carousel

--- src/components/Upcoming/Upcoming.jsx
+++ src/components/Upcoming/Upcoming.jsx
@@ -76,35 +76,34 @@ const Upcoming = ({ upcomingEventsRef }) => {
     if (isMobile) {
         return (
             <div className='background_image_phone' ref={upcomingEventsRef}>
-                <div className='upcoming_bg upcoming__wrapper upcoming_padding'>
+                <div className='app__bg app__wrapper section__padding'>
 
-                    <div className='upcoming_wrapper_info_mobile'>
-                        <img src={upcoming_header} alt="heading_text" className='upcoming_header' />
+                    <div className='app__wrapper_info'>
+                        <img src={upcoming_header} alt="heading_text" className='infoBox_header' />
 
-                        <div className='upcoming_content'>
-                            <div className='upcoming_content_quote upcomingMobile'>
-                                <p className='upcoming_para_mobile'>Book a Private Event with Clayful Hands
-                                    Looking for a unique and creative way to celebrate? Whether it's a hen or stag do,
-                                    team-building session, or Christmas party, Clayful Hands offers tailored pottery experiences
-                                    just for you. Choose from hand-building, pottery painting, or even a combination of both,
-                                    including the chance to try your hand at the potter's wheel.
+                        <div className='app__chef-content'>
+                            <div className='app__chef-content-quote infoBoxMobile'>
+                                <p className='infoBox_para_mobile'>Upcoming Pottery Events: Get Creative with Clayful Hands!
+                                Discover our latest workshops and experiences—perfect for all skill levels & abilities!
                                 </p>
                             </div>
-                            <p className='upcoming_para_mobile upcomingMobile'>For more information on pricing and availability, get in touch with us today and let's plan an unforgettable event!</p>
                         </div>
 
-                        <div className="gallery-container">
-                            {galleryImages.map((item, index) => (
-                                <div key={index} className="gallery-item flex__center">
-                                    <img src={item.image} alt={item.name} className="gallery-image" onClick={() => openWebsite(item.url)} />
-                                    <BsInstagram className="gallery__image-icon" onClick={() => openWebsite(item.url)} style={{ color: "var(--color-jess-green)" }} />
-                                    <div className="image-details">
-                                        <h3 className="image-title">{item.title}</h3>
-                                        <p className="image-name">{item.name}</p>
-                                        <p className="image-price">{item.price}</p>
-                                    </div>
+                        <div className='app__chef-sign'>
+                            <div className="app__gallery-images" style={{ marginTop: '0rem' }}>
+                                <div className="app__gallery-images_container" ref={scrollRef}>
+                                    {galleryImages.map((item, index) => (
+                                        <div className="app__gallery-images_card flex__center" key={`gallery_image-${index + 1}`}>
+                                            <img src={item.image} alt="gallery" onClick={() => openWebsite(item.url)}  style={{ color: "var(--color-jess-green)" }}/>
+                                            {/* <BsInstagram className="gallery__image-icon" onClick={() => openWebsite("https://www.instagram.com/clayfulhands/")} /> */}
+                                        </div>
+                                    ))}
                                 </div>
-                            ))}
+                                <div className="app__gallery-images_arrows">
+                                    <BsArrowLeftShort transform="shrink-6" className="gallery__arrow-icon" onClick={() => scroll('left')} />
+                                    <BsArrowRightShort className="gallery__arrow-icon" onClick={() => scroll('right')} />
+                                </div>
+                            </div>
                         </div>
                     </div>
 
@@ -117,14 +116,9 @@ const Upcoming = ({ upcomingEventsRef }) => {
                 <div className='upcoming_wrapper_info upcoming'>
                     <img src={upcoming_header} alt="heading_text" className='upcoming_header' />
 
-                    <p className='upcoming_para'>Book a Private Event with Clayful Hands
-                        Looking for a unique and creative way to celebrate? Whether it's a hen or stag do,
-                        team-building session, or Christmas party, Clayful Hands offers tailored pottery experiences
-                        just for you. Choose from hand-building, pottery painting, or even a combination of both,
-                        including the chance to try your hand at the potter's wheel.
+                    <p className='upcoming_para upcoming'>Upcoming Pottery Events: Get Creative with Clayful Hands!
+                    Discover our latest workshops and experiences—perfect for all skill levels & abilities!
                     </p>
-                    <p className='upcoming_para upcoming'>For more information on pricing and availability, get in touch with us today and let's plan an unforgettable event!</p>
-
                 </div>
 
                 <div className="upcoming_gallery_images">
