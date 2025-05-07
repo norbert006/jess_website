0a47d4952f17791d207c1270ed617ce2707f5bd5
Norbert Blazejewski (ZJAC006) <ZJAC006@live.rhul.ac.uk>
2025-05-07 19:41:39 +0100

    Starting fresh

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import './index.css';
-import App from './App';
-
-// Render the App component by getting the "root" from index.html
-ReactDOM.render(<App />, document.getElementById('root'))
