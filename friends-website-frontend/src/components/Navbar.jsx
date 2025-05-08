669bfa25b61d4d0d06b13c40a6c2f0055e4559af
Norbert Blazejewski (ZJAC006) <ZJAC006@live.rhul.ac.uk>
2025-05-08 19:59:15 +0100

    Adding comments to Navbar

--- friends-website-frontend/src/components/Navbar.jsx
+++ friends-website-frontend/src/components/Navbar.jsx
@@ -6,6 +6,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFacebook, faInstagram } from '@fortawesome/free-brands-svg-icons';
 
 const Navbar = () => {
+  // Scrolls smoothly to the contact section
   const scrollToContact = () => {
     const contactSection = document.getElementById('contact');
     contactSection?.scrollIntoView({ behavior: 'smooth' });
@@ -13,21 +14,29 @@ const Navbar = () => {
 
   return (
     <div className="navbar">
+      
+      {/* Background image that fills the navbar area */}
       <img src={HeaderImage} alt="Header Background" className="header-image" />
 
+      {/* Overlay for logo, nav buttons, and icons */}
       <div className="navbar-overlay">
         <img src={TransparentLogo} alt="Logo" className="logo" />
 
+        {/* Right side of navbar: contact button and social icons */}
         <div className="nav-right">
           <button className="contact-button" onClick={scrollToContact}>
             CONTACT US
           </button>
+
+          {/* Facebook icon linking to Facebook */}
           <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
             <FontAwesomeIcon icon={faFacebook} className="social-icon" />
           </a>
+          {/* Instagram icon linking to Instagram */}
           <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
             <FontAwesomeIcon icon={faInstagram} className="social-icon" />
           </a>
+          
         </div>
       </div>
     </div>
