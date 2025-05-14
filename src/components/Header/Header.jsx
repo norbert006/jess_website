4795153712a518ea0b269c95854e3ed981b5d27f
Norbert Blazejewski (ZJAC006) <ZJAC006@live.rhul.ac.uk>
2025-05-14 20:25:54 +0100

    Updated buttons

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,36 +2,41 @@
 import React from 'react';
 import './Header.css';
 import importedImage from "../../assets/sub_header_finger.webp"
-import importedLabelImage from "../../assets/button1.svg"
+import Button_pottery_courses from "../../assets/Button_pottery_courses.svg"
+import Button_pop_up_events from "../../assets/Button_pop_up_events.svg"
+import Button_kids_pottery from "../../assets/Button_kids_pottery.svg"
+import Button_private_booking from "../../assets/Button_private_booking.svg"
+import Button_studio_membership from "../../assets/Button_studio_membership.svg"
+import Button_pottery_painting from "../../assets/Button_pottery_painting.svg"
 
 const activities = [
     {
-        labelImage: importedLabelImage,
+        labelImage: Button_pottery_courses,
         image: importedImage,
         className: 'dark'
     },
     {
-        labelImage: importedLabelImage,
+        labelImage: Button_kids_pottery,
         image: importedImage,
         className: 'green'
     },
     {
-        labelImage: importedLabelImage,
+        labelImage: Button_pottery_painting,
         image: importedImage,
         className: 'pink'
     },
     {
-        labelImage: importedLabelImage,
+        labelImage: Button_studio_membership,
         image: importedImage,
         className: 'pink'
     },
     {
-        labelImage: importedLabelImage,
+        labelImage: Button_pop_up_events,
         image: importedImage,
         className: 'dark'
     },
     {
-        labelImage: importedLabelImage,
+        labelImage: Button_private_booking,
         image: importedImage,
         className: 'green'
     },
