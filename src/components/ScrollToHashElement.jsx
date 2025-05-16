8f3fafadb38c5643c2c734f975b73eb0fa72916a
Norbert Blazejewski (ZJAC006) <ZJAC006@live.rhul.ac.uk>
2025-05-16 12:34:25 +0100

    Improved scrolltohashelement so it does not always scroll down to the contact us section when for a example a user refreshes their browser

--- src/components/ScrollToHashElement.jsx
+++ src/components/ScrollToHashElement.jsx
@@ -1,20 +1,29 @@
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 import { useLocation } from 'react-router-dom';
 
 const ScrollToHashElement = () => {
     const location = useLocation();
-    console.log("location.hash = ", location.hash)
-    useEffect(() => {
-        if (location.hash === "#contact_us") {
-            const timeout = setTimeout(() => {
-                const element = document.querySelector(location.hash);
-                if (element) {
-                    element.scrollIntoView({ behavior: 'smooth' });
-                }
-            }, 250);
+    const { pathname } = useLocation();
+    const previousPath = useRef(null);
 
-            return () => clearTimeout(timeout);
+    console.log(previousPath.current?.startsWith('/#contact_us'))
+    useEffect(() => {
+        const navEntries = performance.getEntriesByType("navigation");
+        if (navEntries.length > 0 && navEntries[0].type === "reload") {
+            return;
+        } else {
+            if (location.hash === "#contact_us") {
+                const timeout = setTimeout(() => {
+                    const element = document.querySelector(location.hash);
+                    if (element) {
+                        element.scrollIntoView({ behavior: 'smooth' });
+                    }
+                }, 250);
+
+                return () => clearTimeout(timeout);
+            }
         }
+
     }, [location]);
 
 };
