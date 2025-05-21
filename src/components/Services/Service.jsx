0330b3b97ff4ba898b7e50ea5e881afd90881a54
Norbert Blazejewski (ZJAC006) <ZJAC006@live.rhul.ac.uk>
2025-05-21 22:42:59 +0100

    Added a service section for the rest of the services that are on offer

--- src/components/Services/Service.jsx
+++ src/components/Services/Service.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useNavigate, useParams } from 'react-router-dom';
 
-import importedImage from "../../assets/sub_header_finger.webp"
+import importedImage from "../../assets/Button_kids_pottery.svg"
 import "./Service.css";
 
 const ServiceComponent = ({ services }) => {
@@ -15,15 +15,13 @@ const ServiceComponent = ({ services }) => {
 
     // Find the service that matches the urlName
     var service = services.find(s => s.urlName === 'pottery_courses');
-    console.log("First service = ", service.urlName)
 
-    console.log("img1 ",service.img1)
     if (service.urlName === 'pottery_courses' && serviceName === 'pottery_courses') {
         return (
             <div className="service-container">
 
                 <div key={service._id}>
-                    <h1 className="main-title">{service.serviceName}</h1>
+                    <img className="main-title" src={service.mainTitle} alt="main-title" />
                     <p className="intro">{service.intro}</p>
 
                     <div className="section">
@@ -40,10 +38,10 @@ const ServiceComponent = ({ services }) => {
                         <div className="image-grid">
                             <img src={service.img1} alt="img1" />
                             <div className="row">
-                                <img className="top-bottom-images" src={importedImage} alt="img2" />
-                                <img src={importedImage} alt="img3" />
+                                <img className="top-bottom-images" src={service.img2} alt="img2" />
+                                <img src={service.img3} alt="img3" />
                             </div>
-                            <img src={importedImage} alt="img4" />
+                            <img src={service.img4} alt="img4" />
                         </div>
                         <div className="text-blocks">
                             <div className="text-block">
@@ -64,20 +62,19 @@ const ServiceComponent = ({ services }) => {
     }
 
     service = services.find(s => s.urlName === 'kids_pottery');
-    console.log("Second service = ", service.urlName)
 
     if (service.urlName === 'kids_pottery' && serviceName === 'kids_pottery') {
         return (
             <div className="service-container">
 
                 <div key={service._id}>
-                    <h1 className="main-title" style={{backgroundColor: '#99B698'}}>{service.serviceName}</h1>
+                    <img className="main-title" src={service.mainTitle} alt="main-title" />
                     <p className="intro">{service.intro}</p>
 
                     <div className="section">
                         <div className="text-blocks">
-                            <div className="text-block">
-                                <h2>{service.subtitle1}</h2>
+                            <div className="text-block" >
+                                <h2 >{service.subtitle1}</h2>
                                 <p dangerouslySetInnerHTML={{ __html: service.para1 }}>{ }</p>
                             </div>
                             <div className="text-block">
@@ -90,12 +87,176 @@ const ServiceComponent = ({ services }) => {
                             </div>
                         </div>
                         <div className="image-grid">
-                            <img src={importedImage} alt="img1" />
+                            <img src={service.img1} alt="img1" />
                             <div className="row">
-                                <img className="top-bottom-images" src={importedImage} alt="img2" />
-                                <img src={importedImage} alt="img3" />
+                                <img className="top-bottom-images" src={service.img2} alt="img2" />
+                                <img src={service.img3} alt="img3" />
                             </div>
-                            <img src={importedImage} alt="img4" />
+                            <img src={service.img4} alt="img4" />
+                        </div>
+                    </div>
+                </div>
+
+                <button className="back-button" onClick={handleNavigate}>BACK TO HOME</button>
+            </div>
+        );
+    }
+
+    service = services.find(s => s.urlName === 'pottery_painting');
+
+    if (service.urlName === 'pottery_painting' && serviceName === 'pottery_painting') {
+        return (
+            <div className="service-container">
+
+                <div key={service._id}>
+                    <img className="main-title" src={service.mainTitle} alt="main-title" />
+                    <p className="intro">{service.intro}</p>
+
+                    <div className="section">
+                        <div className="text-blocks">
+                            <div className="text-block" >
+                                <h2 >{service.subtitle1}</h2>
+                                <p dangerouslySetInnerHTML={{ __html: service.para1 }}>{ }</p>
+                            </div>
+                            <div className="text-block">
+                                <h2>{service.subtitle2}</h2>
+                                <p dangerouslySetInnerHTML={{ __html: service.para2 }}>{ }</p>
+                            </div>
+                            <div className="text-block">
+                                <h2>{service.subtitle3}</h2>
+                                <p dangerouslySetInnerHTML={{ __html: service.para3 }}>{ }</p>
+                            </div>
+                        </div>
+                        <div className="image-grid">
+                            <img src={service.img1} alt="img1" />
+                            <div className="row">
+                                <img className="top-bottom-images" src={service.img2} alt="img2" />
+                                <img src={service.img3} alt="img3" />
+                            </div>
+                            <img src={service.img4} alt="img4" />
+                        </div>
+                    </div>
+                </div>
+
+                <button className="back-button" onClick={handleNavigate}>BACK TO HOME</button>
+            </div>
+        );
+    }
+
+    service = services.find(s => s.urlName === 'studio_membership');
+
+    if (service.urlName === 'studio_membership' && serviceName === 'studio_membership') {
+        return (
+            <div className="service-container">
+
+                <div key={service._id}>
+                    <img className="main-title" src={service.mainTitle} alt="main-title" />
+                    <p className="intro">{service.intro}</p>
+
+                    <div className="section">
+                        <div className="text-blocks">
+                            <div className="text-block" >
+                                <h2 >{service.subtitle1}</h2>
+                                <p dangerouslySetInnerHTML={{ __html: service.para1 }}>{ }</p>
+                            </div>
+                            <div className="text-block">
+                                <h2>{service.subtitle2}</h2>
+                                <p dangerouslySetInnerHTML={{ __html: service.para2 }}>{ }</p>
+                            </div>
+                            <div className="text-block">
+                                <h2>{service.subtitle3}</h2>
+                                <p dangerouslySetInnerHTML={{ __html: service.para3 }}>{ }</p>
+                            </div>
+                        </div>
+                        <div className="image-grid">
+                            <img src={service.img1} alt="img1" />
+                            <div className="row">
+                                <img className="top-bottom-images" src={service.img2} alt="img2" />
+                                <img src={service.img3} alt="img3" />
+                            </div>
+                            <img src={service.img4} alt="img4" />
+                        </div>
+                    </div>
+                </div>
+
+                <button className="back-button" onClick={handleNavigate}>BACK TO HOME</button>
+            </div>
+        );
+    }
+
+    service = services.find(s => s.urlName === 'pop_up_events');
+
+    if (service.urlName === 'pop_up_events' && serviceName === 'pop_up_events') {
+        return (
+            <div className="service-container">
+
+                <div key={service._id}>
+                    <img className="main-title" src={service.mainTitle} alt="main-title" />
+                    <p className="intro">{service.intro}</p>
+
+                    <div className="section">
+                        <div className="text-blocks">
+                            <div className="text-block" >
+                                <h2 >{service.subtitle1}</h2>
+                                <p dangerouslySetInnerHTML={{ __html: service.para1 }}>{ }</p>
+                            </div>
+                            <div className="text-block">
+                                <h2>{service.subtitle2}</h2>
+                                <p dangerouslySetInnerHTML={{ __html: service.para2 }}>{ }</p>
+                            </div>
+                            <div className="text-block">
+                                <h2>{service.subtitle3}</h2>
+                                <p dangerouslySetInnerHTML={{ __html: service.para3 }}>{ }</p>
+                            </div>
+                        </div>
+                        <div className="image-grid">
+                            <img src={service.img1} alt="img1" />
+                            <div className="row">
+                                <img className="top-bottom-images" src={service.img2} alt="img2" />
+                                <img src={service.img3} alt="img3" />
+                            </div>
+                            <img src={service.img4} alt="img4" />
+                        </div>
+                    </div>
+                </div>
+
+                <button className="back-button" onClick={handleNavigate}>BACK TO HOME</button>
+            </div>
+        );
+    }
+
+service = services.find(s => s.urlName === 'private_booking');
+
+    if (service.urlName === 'private_booking' && serviceName === 'private_booking') {
+        return (
+            <div className="service-container">
+
+                <div key={service._id}>
+                    <img className="main-title" src={service.mainTitle} alt="main-title" />
+                    <p className="intro">{service.intro}</p>
+
+                    <div className="section">
+                        <div className="text-blocks">
+                            <div className="text-block" >
+                                <h2 >{service.subtitle1}</h2>
+                                <p dangerouslySetInnerHTML={{ __html: service.para1 }}>{ }</p>
+                            </div>
+                            <div className="text-block">
+                                <h2 dangerouslySetInnerHTML={{ __html: service.subtitle2 }}>{ }</h2>
+                                <p dangerouslySetInnerHTML={{ __html: service.para2 }}>{ }</p>
+                            </div>
+                            <div className="text-block">
+                                <h2>{service.subtitle3}</h2>
+                                <p dangerouslySetInnerHTML={{ __html: service.para3 }}>{ }</p>
+                            </div>
+                        </div>
+                        <div className="image-grid">
+                            <img src={service.img1} alt="img1" />
+                            <div className="row">
+                                <img className="top-bottom-images" src={service.img2} alt="img2" />
+                                <img src={service.img3} alt="img3" />
+                            </div>
+                            <img src={service.img4} alt="img4" />
                         </div>
                     </div>
                 </div>
